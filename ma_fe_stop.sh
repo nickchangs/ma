65112cce82b817e5691f3054c0bee63988102776
hwiewie <hwiewie@gmail.com>
2017-10-08 00:27:04 +0800

    Update ma_fe_stop.sh

--- ma_fe_stop.sh
+++ ma_fe_stop.sh
@@ -10,15 +10,25 @@ else
     #echo "切換ma目錄失敗"
     echo "修改變數MAM失敗"
 fi
+echo "判斷ma.html路徑"
+if [ ! -d "/opt/Htdocs/service" ]; then
+    echo "舊版在/opt/Htdocs/ma"
+    filepathe="/opt/Htdocs/ma/maintain.html"
+    filepatha="/opt/Htdocs/ma/wap_maintain.html"
+else
+    echo "新版在/opt/Htdocs/service"
+    filepathe="/opt/Htdocs/service/maintain.html"
+    filepatha="/opt/Htdocs/service/wap_maintain.html"
+fi
 echo "把維護時間從maintain.html內清除"
-sed -i '/维护時間/d' /opt/Htdocs/ma/maintain.html
+sed -i '/维护時間/d' $filepathe
 if [ $? = 0 ]; then
     echo "maintain.html清除維護時間成功"
 else
     echo "maintain.html清除維護時間失敗"
 fi
 echo "把維護時間從wap_maintain.html內清除"
-sed -i '/维护時間/d' /opt/Htdocs/ma/wap_maintain.html
+sed -i '/维护時間/d' $filepatha
 if [ $? = 0 ]; then
     echo "wap_maintain.html清除維護時間成功"
 else
