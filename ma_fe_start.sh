45ba89974bfacbcc45f82a2f2e47a5957780b15c
hwiewie <hwiewie@gmail.com>
2017-10-08 00:17:56 +0800

    Update ma_fe_start.sh

--- ma_fe_start.sh
+++ ma_fe_start.sh
@@ -18,27 +18,34 @@ else
     echo "切換MAM修改失敗"
     exit
 fi
-
+echo "判斷ma.html路徑"
+if [ ! -d "/opt/Htdocs/service" ]; then
+    echo "舊版在/opt/Htdocs/ma"
+    filepathe="/opt/Htdocs/ma/maintain.html"
+    filepatha="/opt/Htdocs/ma/wap_maintain.html"
+else
+    echo "新版在/opt/Htdocs/service"
+    filepathe="/opt/Htdocs/service/maintain.html"
+    filepatha="/opt/Htdocs/service/wap_maintain.html"
+fi
 echo "判斷main.html與wap_maintain.html是否存在"
-filepathe="/opt/Htdocs/ma/maintain.html"
-filepatha="/opt/Htdocs/ma/wap_maintain.html"
 if [ -e $filepathe ];then
     echo "maintain.html存在，將繼續執行"
 else
     echo "maintain.html不存在！請確認維謢網頁是否存在"
-    #curl -s https://raw.githubusercontent.com/nickchangs/ma/master/maintain.html -o "/opt/Htdocs/ma/maintain.html"
+    #curl -s https://raw.githubusercontent.com/nickchangs/ma/master/maintain.html -o "$filepathe"
     exit
 fi
 if [ -e $filepatha ];then
     echo "wap_maintain.html存在，將繼續執行"
 else
     echo "wap_maintain.html不存在！請確認網謢網頁是否存在"
-    #curl -s https://raw.githubusercontent.com/nickchangs/ma/master/wap_maintain.html -o "/opt/Htdocs/ma/wap_maintain.html"
+    #curl -s https://raw.githubusercontent.com/nickchangs/ma/master/wap_maintain.html -o "$filepatha"
     exit
 fi
 
 echo "把上次維護時間從maintain.html網頁內移掉"
-sed -i '/维护時間/d' /opt/Htdocs/ma/maintain.html
+sed -i '/维护時間/d' $filepathe
 if [ $? = 0 ]; then
     echo "從maintain.html移除上次維護時間成功"
 else
@@ -46,7 +53,7 @@ else
     exit
 fi
 echo "把上次維護時間從wap_maintain.html網頁內移掉"
-sed -i '/维护時間/d' /opt/Htdocs/ma/wap_maintain.html
+sed -i '/维护時間/d' $filepatha
 if [ $? = 0 ]; then
     echo "從wap_maintain.html移除上次維護時間成功"
 else
@@ -56,7 +63,7 @@ fi
 
 echo "維護開始時間：{$stime}，維護停止時間：{$etime}"
 echo "開始寫入維護時間到maintain.html內"
-sed -i '/在线客服/i\<p\>\<span\ class\=\"red\"\>维护時間\ '$stime'\-'$etime'\<\/span\>\<\/p\>' /opt/Htdocs/ma/maintain.html
+sed -i '/在线客服/i\<p\>\<span\ class\=\"red\"\>维护時間\ '$stime'\-'$etime'\<\/span\>\<\/p\>' $filepathe
 if [ $? = 0 ]; then
     echo "寫入維護時間到maintain.html內成功"
 else
@@ -64,7 +71,7 @@ else
     exit
 fi
 echo "開始寫入維護時間到wap_maintain.html內"
-sed -i '/系统正在升级维护中/a\<p\>\<span\ class\=\"red\"\>维护時間\ '$stime'\-'$etime'\<\/span\>\<\/p\>' /opt/Htdocs/ma/wap_maintain.html
+sed -i '/系统正在升级维护中/a\<p\>\<span\ class\=\"red\"\>维护時間\ '$stime'\-'$etime'\<\/span\>\<\/p\>' $filepatha
 if [ $? = 0 ]; then
     echo "寫入維護時間到wap_maintain.html內成功"
 else
