963e6e128198dab0a3b2167e7ca704db4ccab772
nickchangs <nickchangs@gmail.com>
2017-10-07 15:42:17 +0800

    Update ma_fe_start.sh

--- ma_fe_start.sh
+++ ma_fe_start.sh
@@ -6,7 +6,7 @@ if [ $? = 0 ]; then
     echo "切換ma目錄成功"
 else
     echo "切換ma目錄失敗"
-    break
+    exit
 fi
 
 echo "判斷main.html與wap_maintain.html是否存在"
@@ -17,14 +17,14 @@ if [ -e $filepathe ];then
 else
     echo "maintain.html不存在！請確認維謢網頁是否存在"
     #curl -s https://raw.githubusercontent.com/nickchangs/ma/master/maintain.html -o "/opt/Htdocs/ma/maintain.html"
-    break
+    exit
 fi
 if [ -e $filepatha ];then
     echo "wap_maintain.html存在，將繼續執行"
 else
     echo "wap_maintain.html不存在！請確認網謢網頁是否存在"
     #curl -s https://raw.githubusercontent.com/nickchangs/ma/master/wap_maintain.html -o "/opt/Htdocs/ma/wap_maintain.html"
-    break
+    exit
 fi
 
 echo "把上次維護時間從maintain.html網頁內移掉"
@@ -33,7 +33,7 @@ if [ $? = 0 ]; then
     echo "從maintain.html移除上次維護時間成功"
 else
     echo "從maintain.html移除上次維護時間失敗"
-    break
+    exit
 fi
 echo "把上次維護時間從wap_maintain.html網頁內移掉"
 sed -i '/维护時間/d' /opt/Htdocs/ma/wap_maintain.html
@@ -41,7 +41,7 @@ if [ $? = 0 ]; then
     echo "從wap_maintain.html移除上次維護時間成功"
 else
     echo "從wap_maintain.html移除上次維護時間失敗"
-    break
+    exit
 fi
 
 echo "維護開始時間：{$1}，維護停止時間：{$2}"
@@ -51,7 +51,7 @@ if [ $? = 0 ]; then
     echo "寫入維護時間到maintain.html內成功"
 else
     echo "寫入維護時間到maintain.html內失敗"
-    break
+    exit
 fi
 echo "開始寫入維護時間到wap_maintain.html內"
 sed -i '/系统正在升级维护中/a\<p\>\<span\ class\=\"red\"\>维护時間\ '$1'\-'$2'\<\/span\>\<\/p\>' /opt/Htdocs/ma/wap_maintain.html
@@ -59,7 +59,7 @@ if [ $? = 0 ]; then
     echo "寫入維護時間到wap_maintain.html內成功"
 else
     echo "寫入維護時間到wap_maintain.html內失敗"
-    break
+    exit
 fi
 
 echo "重新載入nginx設定檔"
@@ -70,5 +70,5 @@ if [ $? = 0 ]; then
 else
     echo "切換ma目錄失敗"
     echo "失敗！請確認/opt/APP/openresty/nginx/conf/ma/ma.conf內容是否正確！"
-    break
+    exit
 fi
