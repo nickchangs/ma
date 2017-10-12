fc3a2387c2a9460513243deca797f3e39ca8c8ca
nickchangs <nickchangs@gmail.com>
2017-10-12 15:56:53 +0800

    Update geoip_add.sh

--- geoip_add.sh
+++ geoip_add.sh
@@ -10,16 +10,39 @@ fi
 #新增geoip
 if [ $1 == add ]; then
 	sed -i '/^\s*default\ 1/a\          '$2'/32\ 0;' /opt/APP/openresty/nginx/conf/nginx.conf
-fi
-if [ $1 == del ]; then
-        sed -i '/^\s*'$2'\/32\ 0\;/d' /opt/APP/openresty/nginx/conf/nginx.conf
-fi
 if [ $? = 0 ]; then
     echo "新增geoip白名單成功"
 else
-    echo "新增geoip白名單失數"
+    echo "新增geoip白名單失敗"
     exit
 fi
+fi
+#刪除geoip
+if [ $1 == del ]; then
+        sed -i '/^\s*'$2'\/32\ 0\;/d' /opt/APP/openresty/nginx/conf/nginx.conf
+if [ $? = 0 ]; then
+    echo "刪除geoip白名單成功"
+else
+    echo "刪除geoip白名單失敗"
+    exit
+fi
+fi
+#顯示geoip清單
+if [ $1 == show ]; then
+        grep '^\s*[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' /opt/APP/openresty/nginx/conf/nginx.conf
+if [ $? = 0 ]; then
+    echo "顯示geoip白名單成功"
+else
+    echo "顯示geoip白名單失敗"
+    exit
+fi
+fi
+#if [ $? = 0 ]; then
+#    echo "新增geoip白名單成功"
+#else
+#    echo "新增geoip白名單失敗"
+#    exit
+#fi
 #重啟nginx服務
 #service nginx restart
 #if [ $? = 0 ]; then
