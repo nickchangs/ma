73adecf9e37275f8eb31a87a8626a29cd2b4bed8
nickchangs <nickchangs@gmail.com>
2017-10-19 14:32:43 +0800

    Update upgrade_geoip.sh

--- upgrade_geoip.sh
+++ upgrade_geoip.sh
@@ -1,2 +1,2 @@
 geoipdata=`grep GeoIP.dat /opt/APP/openresty/nginx/conf/nginx.conf | awk '{print $2}' | sed -r 's/;//g'`
-wget -O $geoipdata https://raw.githubusercontent.com/hwiewie/OPS/master/GeoIP-106_20170926.dat
+wget -O $geoipdata https://raw.githubusercontent.com/nickchangs/ma/master/GeoIP-106_20171017.dat
