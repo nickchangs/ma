8f38b4313773852b3adcb46235bd338d8fd6f2fa
nickchangs <nickchangs@gmail.com>
2017-10-19 14:32:17 +0800

    Create upgrade_geoip.sh

---
+++ upgrade_geoip.sh
@@ -0,0 +1,2 @@
+geoipdata=`grep GeoIP.dat /opt/APP/openresty/nginx/conf/nginx.conf | awk '{print $2}' | sed -r 's/;//g'`
+wget -O $geoipdata https://raw.githubusercontent.com/hwiewie/OPS/master/GeoIP-106_20170926.dat
